e0391c8e190eb030b3aad67f79669c3093814883
storycode <storycode143.com>
2023-06-09 11:30:30 +0530

    born2

--- pages/_app.js
+++ pages/_app.js
@@ -5,11 +5,16 @@ import {NavBar,Footer} from '../components/componentindex'
 
 
 const MyApp = ({ Component, pageProps }) => (
+
     <div>
         <NavBar/>
+
         <Component {...pageProps} />
+
         <Footer/>
+
         </div>
+        
 );
 
 export default MyApp;
